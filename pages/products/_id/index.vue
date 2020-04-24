9e24e3e5b134b08eaf77567bbf2fd07a611eb48f
tmeftah <sudtechnics@gmail.com>
2020-04-25 01:30:59 +0200

    :construction: add product list / page

---
+++ pages/products/_id/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-overlay :value="loading">
+      <v-progress-circular indeterminate size="64"></v-progress-circular>
+    </v-overlay>
+    name : {{ product.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loading: true,
+      product: {}
+    }
+  },
+  validate({ params }) {
+    // Must be a number
+    return /^\d+$/.test(params.id)
+  },
+
+  mounted() {
+    this.getDataFromApi()
+  },
+  methods: {
+    async getDataFromApi() {
+      this.loading = true
+      console.log(this.$route.params.id)
+      let data = (await this.$axios.get('products/' + this.$route.params.id))
+        .data
+
+      ;(this.loading = false), (this.product = data)
+    }
+  }
+}
+</script>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,3 +1,72 @@
 <template>
-  <div>products page</div>
+  <div>
+    products page
+    <div>
+      <v-card>
+        <v-card-title>
+          <v-text-field
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="products"
+          :options.sync="options"
+          :server-items-length="total"
+          :loading="loading"
+          @click:row="goTo"
+          class="elevation-1"
+        ></v-data-table>
+      </v-card>
+    </div>
+  </div>
 </template>
+<script>
+export default {
+  middleware: 'auth',
+  data() {
+    return {
+      total: 0,
+      desserts: [],
+      loading: true,
+      options: {},
+      headers: [
+        {
+          text: 'name',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        }
+      ],
+      products: []
+    }
+  },
+
+  mounted() {
+    this.getDataFromApi()
+  },
+  methods: {
+    async getDataFromApi() {
+      this.loading = true
+      let data = (
+        await this.$axios.get('products', {
+          params: {
+            page: this.options.page,
+            per_page: this.options.itemsPerPage
+          }
+        })
+      ).data
+
+      ;(this.loading = false),
+        (this.products = data.products),
+        (this.total = data.total)
+    },
+    goTo(value) {
+      this.$router.push({ path: '/products/' + value.id })
+    }
+  }
+}
+</script>

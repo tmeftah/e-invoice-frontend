9abc5588234bc04967d009e26410ff3321ce58f3
tmeftah <sudtechnics@gmail.com>
2020-04-30 02:57:41 +0200

    :construction: add dashbord elements

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,85 @@
 <template>
   <div>
-    <p>home page</p>
+    <v-row align="stretch">
+      <v-col>
+        <v-card height="100%">
+          <v-card-title class="grey lighten-4 subtitle-2 py-2">Recent Activity</v-card-title>
+          <v-list dense>
+            <v-list-item-group>
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>20 hours ago — Invoice modified</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+
+              <v-list-item>
+                <v-list-item-content>
+                  <v-list-item-title>20 hours ago — Invoice created for xxx</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card height="100%">
+          <v-card-title class="grey lighten-4 subtitle-2 py-2">Overview</v-card-title>
+          <v-list dense>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-icon left>mdi-clock</v-icon>Total Outstanding:
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-list-item-action-text>248.50 Euro</v-list-item-action-text>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-icon left>mdi-alert</v-icon>Total Overdue:
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-list-item-action-text>0.00 Euro</v-list-item-action-text>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-icon left>mdi-check</v-icon>Total Collected this Year:
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-list-item-action-text>0.00 Euro</v-list-item-action-text>
+              </v-list-item-action>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <v-icon left>mdi-account-multiple</v-icon>Clients:
+                </v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-list-item-action-text>1</v-list-item-action-text>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row align="stretch">
+      <v-col>
+        <v-card height="100%">
+          <v-card-title
+            class="grey lighten-4 subtitle-2 py-2"
+          >Invoiced / Received (Jan 01 2020 - Apr 28 2020)</v-card-title>
+          <v-sheet color="orange lighten-2" height="300px" tile></v-sheet>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
-  <!-- <div>
-    <div v-if="$store.state.auth">
-      <p>
-        You are authenticated. You can see the
-        <NuxtLink to="/products">products page</NuxtLink>!
-      </p>
-    </div>
-    <p v-else>
-      Please
-      <NuxtLink to="/login">login</NuxtLink>.
-    </p>
-  </div>-->
 </template>
 
 <script>

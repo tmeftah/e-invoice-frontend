2806f55d94676c1f6072c0c88d7f1422b4a9e004
tmeftah <sudtechnics@gmail.com>
2020-05-03 03:19:41 +0200

    add dark mode switch

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,44 @@
 <template>
-  <v-app
-    style="
-  background: #3A1C71;
-  background: -webkit-linear-gradient(to right, #FFAF7B, #D76D77, #3A1C71);
-  background: linear-gradient(to right, #FFAF7B, #D76D77, #3A1C71);
-"
-  >
+  <v-app style="font-family: Roboto;" id="inspire">
     <SideBar />
 
     <v-content>
-      <v-container v-if="isAuthenticated " fluid class="px-6">
+      <v-container v-if="isAuthenticated" class="px-6">
         <nuxt />
       </v-container>
       <v-container v-else fluid fill-height>
         <nuxt />
       </v-container>
     </v-content>
+
     <Footer />
   </v-app>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  import SideBar from '@/components/SideBar.vue'
-  import Footer from '@/components/FooterBar.vue'
-  export default {
-    name: 'DefaultLayout',
-    components: {
-      SideBar,
-      Footer
-    },
-    computed: {
-      ...mapGetters(['isAuthenticated', 'loggedInUser'])
+import { mapGetters } from 'vuex'
+import SideBar from '@/components/SideBar.vue'
+import Footer from '@/components/FooterBar.vue'
+export default {
+  name: 'DefaultLayout',
+  data: () => ({
+    goDark: false
+  }),
+  components: {
+    SideBar,
+    Footer
+  },
+  computed: {
+    ...mapGetters(['isAuthenticated', 'loggedInUser'])
+  },
+  beforeMount() {
+    const theme = localStorage.getItem('useDarkTheme')
+    if (theme) {
+      if (theme == 'true') {
+        this.$vuetify.theme.dark = true
+      } else this.$vuetify.theme.dark = false
+      this.goDark = this.$vuetify.theme.dark
     }
   }
+}
 </script>
--- pages/settings/index.vue
+++ pages/settings/index.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>settings page</div>
+  <div>
+    <v-switch label="Dark Mode" v-model="dark" @change="toggleTheme"></v-switch>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      dark: false
+    }
+  },
+  mounted() {
+    this.dark = this.$vuetify.theme.dark
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.dark = this.dark
+      localStorage.setItem('useDarkTheme', this.$vuetify.theme.dark.toString())
+    }
+  }
+}
+</script>

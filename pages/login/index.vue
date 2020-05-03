41129e97fe4c9dafc0633901ccc71fb39333543f
tmeftah <sudtechnics@gmail.com>
2020-05-03 04:09:34 +0200

    remove default password and  username

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -18,60 +18,60 @@
 </template>
 
 <script>
-  const Cookie = process.client ? require('js-cookie') : undefined
+const Cookie = process.client ? require('js-cookie') : undefined
 
-  export default {
-    layout: 'default',
-    auth: 'guest',
-    data() {
-      return {
-        username: 'admin',
-        password: 'demo',
-        error: null
-      }
-    },
-    computed: {
-      availableLocales() {
-        return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
-      }
-    },
-    methods: {
-      async login() {
-        await this.$auth
-          .loginWith('local', {
-            data: {
-              username: this.username,
-              password: this.password
-            }
-          })
-          .then(resp => {
-            this.$auth.setToken('local', 'Bearer ' + resp.data.access_token)
-            this.$auth.setRefreshToken('local', resp.data.refresh_token)
-            this.$axios.setHeader(
-              'Authorization',
-              'Bearer ' + resp.data.access_token
-            )
-            this.$auth.ctx.app.$axios.setHeader(
-              'Authorization',
-              'Bearer ' + resp.data.access_token
-            )
-            this.$store.commit('firtsLogin', true)
-            this.$router.push(this.localePath('/'))
-          })
-          .catch(error => {
-            if (!error.response) {
-              // network error
-              this.$notifier.showMessage({
-                content: 'Server not responding',
-                color: 'error'
-              })
+export default {
+  layout: 'default',
+  auth: 'guest',
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: null
+    }
+  },
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
+    }
+  },
+  methods: {
+    async login() {
+      await this.$auth
+        .loginWith('local', {
+          data: {
+            username: this.username,
+            password: this.password
+          }
+        })
+        .then(resp => {
+          this.$auth.setToken('local', 'Bearer ' + resp.data.access_token)
+          this.$auth.setRefreshToken('local', resp.data.refresh_token)
+          this.$axios.setHeader(
+            'Authorization',
+            'Bearer ' + resp.data.access_token
+          )
+          this.$auth.ctx.app.$axios.setHeader(
+            'Authorization',
+            'Bearer ' + resp.data.access_token
+          )
+          this.$store.commit('firtsLogin', true)
+          this.$router.push(this.localePath('/'))
+        })
+        .catch(error => {
+          if (!error.response) {
+            // network error
+            this.$notifier.showMessage({
+              content: 'Server not responding',
+              color: 'error'
+            })
 
-              this.errorStatus = 'Error: Network Error'
-            } else {
-              this.errorStatus = error.response.data.message
-            }
-          })
-      }
+            this.errorStatus = 'Error: Network Error'
+          } else {
+            this.errorStatus = error.response.data.message
+          }
+        })
     }
   }
+}
 </script>
